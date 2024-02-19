bfd46afac83d6a239508e680626c7e108de3c76d
David Zaki <davidzaki77@gmail.com>
2024-02-19 09:09:05 +0200

    add single product page and shop page and cart page

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // const urlParams = new URLSearchParams(window.location.search);
 // const username = urlParams.get('userName');
-
+//ankor in pro
+let aLimitFeatuerUser = document.querySelectorAll('.pro-container .pro a i');
+console.log(aLimitFeatuerUser);
+for (let i = 0; i < aLimitFeatuerUser.length; i++) {
+  aLimitFeatuerUser[i].addEventListener('click', (e) => {
+    alert('please login in to get more feature');
+    e.preventDefault();
+  });
+}
 // document.addEventListener('DOMContentLoaded', function () {
 //   let div7 = document.getElementById('div7');
 //   let xhr = new XMLHttpRequest();
@@ -26,10 +34,32 @@
 //           <h4>$${item.price}</h4>
 //         </div>
 //         <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-
 //         `;
 //         div7.insertAdjacentElement('afterend', newDiv);
 //       }
 //     }
 //   };
 // });
+
+//navbar hum burger manu clickable
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const closeNavbar = document.getElementById('close');
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  });
+}
+if (closeNavbar) {
+  closeNavbar.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
+
+// Disable the back button by replacing the current page with itself
+history.replaceState(null, null, location.href);
+// Listen for the back button event
+window.onpopstate = function (event) {
+  // If the user tries to go back, push a new state and replace the current page with itself again
+  history.pushState(null, null, location.href);
+};
